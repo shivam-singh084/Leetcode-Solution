8bf39f4e0e9a799122a9174aa23092feec0eab43
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-01-24 03:21:01 +0530

    Time: 7 ms (3.08%), Space: 50 MB (91.67%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,12 +4,12 @@ public:
         int cnt = 0;
         int curr = 0;
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == 1){
+            if(nums[i] != 1){
+                cnt = 0;
+                curr = max(curr, cnt);
+            }else{
                 cnt++;
                 curr = max(curr, cnt);
-            }
-            else{
-                cnt = 0;
             }
         }
         return curr;
