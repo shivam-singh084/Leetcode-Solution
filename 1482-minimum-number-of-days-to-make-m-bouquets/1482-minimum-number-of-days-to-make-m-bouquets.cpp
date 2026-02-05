4f5b22f5db24643a5d97968cfef7f2f9ac381bed
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-02-05 23:58:52 +0530

    Time: 23 ms (54.77%), Space: 70.6 MB (45.76%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    bool canMake(vector<int>& bloomDay, int m, int k, int day) {
-        int bouquets = 0;
-        int flowers = 0;
-
-        for (int i = 0; i < bloomDay.size(); i++) {
-            if (bloomDay[i] <= day) {
-                flowers++;
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
-                }
-            } else {
-                flowers = 0;
+    int canMakeBouquet(vector<int>& bloomDay, int mid, int k){
+        int cnt = 0;
+        int consecutive_cnt = 0;
+        for(int i = 0; i < bloomDay.size(); i++){
+            if(bloomDay[i] <= mid){
+                consecutive_cnt++;
+            }else{
+                consecutive_cnt = 0;
+            }
+            if(consecutive_cnt == k){
+                cnt++;
+                consecutive_cnt = 0;
             }
         }
-        return bouquets >= m;
+        return cnt;
     }
-
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        
-        // Edge case
-        if ((long long)m * k > n) return -1;
+        int st = 0;
+        int end = *max_element(bloomDay.begin(), bloomDay.end());
+        int minDays = -1;
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            
 
-        int low = *min_element(bloomDay.begin(), bloomDay.end());
-        int high = *max_element(bloomDay.begin(), bloomDay.end());
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (canMake(bloomDay, m, k, mid)) {
-                ans = mid;
-                high = mid - 1;  // try smaller day
-            } else {
-                low = mid + 1;
+            if(canMakeBouquet(bloomDay, mid, k) >= m){
+                minDays = mid;
+                end = mid-1;
+            }else{
+                st = mid + 1;
             }
+
         }
-        return ans;
+        return minDays;
     }
 };
