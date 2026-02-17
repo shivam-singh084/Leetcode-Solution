171e58107df5eafa66a3ef828c6070555f629058
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-02-17 12:22:06 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43.82%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,7 +4,7 @@ public:
         if(n == 0) return 1;
         if(n < 0) return solve(1/x, -n);
         if(n % 2 == 0) return solve(x*x, n/2);
-        return x * solve(x*x, n/2);
+        return x * solve(x*x,(n-1)/2);
     }
     double myPow(double x, int n) {
         return solve(x, (long) n);
