590859861ef219bc8441d4992bbc29e165189c52
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-02-24 21:39:42 +0530

    Time: 19 ms (94.05%), Space: 71.1 MB (73.44%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
-
-        
-       for(int i = 0; i < n; i++){
-          for(int j = i +1 ; j < n; j ++){
-            if(nums[i] == nums[i+1]){
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] == nums[i-1]){
                 return true;
             }
-            
-          }
-            
         }
         return false;
-       
     }
 };
