be84f8fb0ab317b0a13361a576b4ea33e4378641
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-03-19 17:21:22 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.37%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> mp;
-
-        for(int i : nums){
-            mp[i]++;
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            ans = ans ^ nums[i];
         }
-        for(auto it : mp){
-            if(it.second == 1){
-                return it.first;
-            }
-        }
-        return -1;
+        return ans;
     }
 };
