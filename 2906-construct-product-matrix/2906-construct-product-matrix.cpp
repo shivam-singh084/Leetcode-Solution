cc8a929174490aff70dfd0f3233dc8a13a83adfe
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-03-24 12:28:58 +0530

    Time: 27 ms (87.45%), Space: 132.1 MB (80.07%) - LeetHub

---
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int mod = 12345;
+
+        vector<vector<int>> p(n, vector<int>(m));
+        
+        long long suffix = 1;
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
+                p[i][j] = suffix;
+                suffix = (suffix * grid[i][j]) % mod;
+            }
+        }
+
+        long long prefix = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                p[i][j] = (p[i][j] * prefix) % mod;
+                prefix = (prefix * grid[i][j]) % mod;
+            }
+        }
+        return p;
+    }
+};
