a2e2b6bc775f2e85afaebca2294fbca3716f07c7
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-04-11 11:33:19 +0530

    Time: 355 ms (78.75%), Space: 343.4 MB (74.5%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -11,7 +11,7 @@ public:
             if(it.second.size() >= 3){
                 vector<int> &z = it.second;
                 for(int t = 0; t+2 < z.size(); t++){
-                    ans = min(ans, 2*abs(z[t+2]-z[t]));
+                    ans = min(ans, 2 * abs(z[t+2] - z[t]));
                 }
             }
         }
