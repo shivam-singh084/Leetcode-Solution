3ae19c9eb0871d3e6dab9e63458f78efdce4b9fc
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-04-18 12:36:14 +0530

    Time: 202 ms (36.56%), Space: 134.1 MB (40.19%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -8,12 +8,12 @@ public:
         for(int j = 0; j < n; j++){
             string s = to_string(nums[j]);
             reverse(s.begin(), s.end());
-            int rev = stoi(s);
+    
             if(mp.find(nums[j]) != mp.end()){
                 mini = min(mini, j - mp[nums[j]]);
             }
             
-            mp[rev] = j;
+            mp[stoi(s)] = j;
         }
 
         return (mini == INT_MAX) ? -1 : mini;
