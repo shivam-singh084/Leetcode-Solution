e1a8cba302ddb280f034553fb6125ad53f409bdc
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-05-04 13:45:23 +0530

    Time: 0 ms (100%), Space: 10.1 MB (92.7%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -8,9 +8,9 @@ public:
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-
         for(int i = 0; i < n; i++){
             reverse(matrix[i].begin(), matrix[i].end());
         }
+        
     }
 };
