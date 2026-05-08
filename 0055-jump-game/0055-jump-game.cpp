64b8ab299c647d770e1c3c68dfb065540a1201f4
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-05-08 22:58:13 +0530

    Time: 0 ms (100%), Space: 52.4 MB (55.49%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int n, int idx, vector<int>& dp){
-
-        if(idx == n-1) return true;
-
-        if(dp[idx] != -1) return dp[idx];
-
-        for(int jump = 1; jump <= nums[idx]; jump++){
-            if(solve(nums, n, idx + jump, dp)){
-                return dp[idx] = true;
-            }
-        }
-        return dp[idx] = false;
-        
-    }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
+        int first = nums[0];
+        for(int i = 0; i < n; i++){
+            first = max(first , i + nums[i]);
 
-        return solve(nums, n, 0, dp);
+            if(first >= n-1) return true;
+            
+            if(i == first) return false;
+        }
+        return true;
+        
     }
 };
