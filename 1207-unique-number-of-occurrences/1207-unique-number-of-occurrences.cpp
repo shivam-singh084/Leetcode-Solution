cd76cf044443efd279c026c67752f46c2e6bd27d
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-07-16 01:04:24 +0530

    Time: 0 ms (100%), Space: 12 MB (39.99%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, int> mp;
+        for(int i = 0; i < n; i++){
+            mp[arr[i]]++;
+        }
+        unordered_set<int> st;
+        for(auto it : mp){
+            if(st.count(it.second)) return false;
+            st.insert(it.second);
+        }
+        return true;
+    }
+};
