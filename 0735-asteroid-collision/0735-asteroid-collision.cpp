b64e862c649a8efdc4436dbdbc1385473fb581c8
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-07-19 02:00:57 +0530

    Time: 3 ms (28.56%), Space: 22.3 MB (19.36%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int n = asteroids.size();
+        stack<int> st;
+
+        for(int i = 0; i < n; i++){
+            if(asteroids[i] > 0){
+                st.push(asteroids[i]);
+            }
+            else{
+                while(!st.empty() && st.top() > 0 && abs(asteroids[i]) > st.top()) st.pop();
+                if(st.empty() || st.top() < 0) st.push(asteroids[i]);
+                else if(abs(asteroids[i]) == st.top()) st.pop();
+            }
+        }
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
