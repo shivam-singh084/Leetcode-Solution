1a9a4f0b1f5b0e7c3d10f90d2f55b0a9aaecd09e
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-07-20 17:39:11 +0530

    Time: 0 ms (100%), Space: 18.2 MB (66.35%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    void reverse(vector<int>& ans, int i, int j){
+    void reverse(vector<vector<int>>& grid, int i, int j){
+        int col = grid[0].size();
         while(i < j){
-            swap(ans[i], ans[j]);
+            swap(grid[i/col][i%col], grid[j/col][j%col]);
             i++;
             j--;
         }
     }
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int row = grid.size();
+        int col = grid[0].size();
 
-        
-        vector<int> ans;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                ans.push_back(grid[i][j]);
-            }
-        }
-        
-        int size = m*n;
-        k = k % size;
-        reverse(ans, 0, size-1);
-        reverse(ans, 0, k-1);
-        reverse(ans, k, size-1);
-
-        int idx = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                grid[i][j] = ans[idx];
-                idx++;
-            }
-        }
+        int n = row * col;
+        k = k % n;
+        reverse(grid, 0, n-1);
+        reverse(grid, 0, k-1);
+        reverse(grid, k, n-1);
 
         return grid;
     }
