0ef9f6528ca007bd00b69e4bc976451ddfad1103
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-08-01 12:49:25 +0530

    Time: 0 ms (100%), Space: 9.4 MB (99.74%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+    int dp[23][23];
     int solve(vector<int>& nums, int i, int j){
         if(i > j) return 0;
         if(i == j) return nums[i];
 
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
         int take_i = nums[i] - solve(nums, i+1, j);
         int take_j = nums[j] - solve(nums, i, j-1);
 
-        return max(take_i, take_j);
+        return dp[i][j] = max(take_i, take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        memset(dp, -1, sizeof(dp));
         return solve(nums, 0, n-1) >= 0;
     }
 };
