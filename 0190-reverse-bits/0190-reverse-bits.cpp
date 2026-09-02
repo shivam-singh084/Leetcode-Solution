5ff67f2603cc6906d4b673b8fd407bdbd41c0e3c
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-09-02 13:34:23 +0530

    Time: 0 ms (100%), Space: 8.3 MB (45.51%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        if(n == 0) return 0;
+
+        int res = 0;
+        for(int i = 1; i <= 32; i++){
+            res = res << 1;
+            res = res | (n & 1);
+            n = n >> 1;
+        }
+        return res;
+    }
+};
